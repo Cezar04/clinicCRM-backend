8534579f5585bed1758b404aca9176869efa5c1d
cezar04 <iancu.m.cezar@gmail.com>
2022-01-20 00:31:48 +0200

    crosOrigin added to controllers

--- src/main/java/com/crm/clinicCrm/GDPR/GdprController.java
+++ src/main/java/com/crm/clinicCrm/GDPR/GdprController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/gdpr")
 public class GdprController {
